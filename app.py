6ef996c48e7794d3b979a80a7c00b12b2e64985f
juddyhuddy <justinl732@nycstudents.net>
2026-03-10 09:08:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -127,9 +127,11 @@ maison_margiela_item = [{
     "description": "The Tabi ballerina flats reinterpret the Maison’s iconic split-toe design in a delicate ballet silhouette. Made in supple leather with a slim sole, they embody avant-garde tradition and timeless elegance."
 }
 ]
-choice = int(input("What is the item number"))
+
+choice = int(input("Item Number?"))
 for index, item in enumerate(maison_margiela_item):
     print(index, ":", item["name"])
+print(maison_margiela_item[choice])
     
     
     
